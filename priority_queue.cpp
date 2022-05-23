d48889c80c2e7dc25ad7ee85ed3f337ddee7140e
flying wizard <100211110+Ankit4725@users.noreply.github.com>
2022-05-23 11:08:25 +0530

    Create priority_queue.cpp

---
+++ priority_queue.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <queue>
+using namespace std;
+int main() {
+  // max heap
+  priority_queue<int> maxi;
+
+  // min - heap
+  priority_queue<int, vector<int>, greater<int>> mini;
+  maxi.push(1);
+  maxi.push(2);
+  maxi.push(3);
+  maxi.push(0);
+
+  int n = maxi.size();
+  for (int i = 0; i < n; i++) {
+    cout << maxi.top() << " " << endl;
+    maxi.pop();
+  }
+  cout << "empty or not " << maxi.empty() << endl;
+
+  mini.push(5);
+  mini.push(1);
+  mini.push(0);
+  mini.push(4);
+  mini.push(3);
+
+  int c = mini.size();
+  for (int i = 0; i < c; i++) {
+    cout << mini.top() << " " << endl;
+    mini.pop();
+  }
+  cout << "empty or not " << maxi.empty() << endl;
+}
